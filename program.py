5399a9755946f4ffbcecf09a80d2aedfef5f0f2a
Reinis487 <reinis.berkis@valmiera.edu.lv>
2024-12-12 13:23:55 +0200

    Pievienota datuma un laika funkcionalitāte

--- program.py
+++ program.py
@@ -2,4 +2,17 @@ def sveiciens(vards):
     return f"Čau, {vards}!"
 
 if __name__ == "__main__":
-    print(sveiciens("Pasaule"))
+    lietotaja_vards = input("Ievadi savu vārdu: ")
+    print(sveiciens(lietotaja_vards))
+
+import datetime
+def sveiciens(vards):
+    return f"Čau, {vards}!"
+
+def datuma_paradisana():
+    return f"Šodien ir {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
+
+if __name__ == "__main__":
+    lietotaja_vards = input("Ievadi savu vārdu: ")
+    print(sveiciens(lietotaja_vards))
+    print(datuma_paradisana())
