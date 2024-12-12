108c30cfae9d490955661d78633375d95a9797a0
Reinis487 <reinis.berkis@valmiera.edu.lv>
2024-12-12 13:32:35 +0200

    atjaunots sveiciens

--- program.py
+++ program.py
@@ -1,18 +1,2 @@
 def sveiciens(vards):
-    return f"Čau, {vards}!"
-
-if __name__ == "__main__":
-    lietotaja_vards = input("Ievadi savu vārdu: ")
-    print(sveiciens(lietotaja_vards))
-
-import datetime
-def sveiciens(vards):
-    return f"Čau, {vards}!"
-
-def datuma_paradisana():
-    return f"Šodien ir {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
-
-if __name__ == "__main__":
-    lietotaja_vards = input("Ievadi savu vārdu: ")
-    print(sveiciens(lietotaja_vards))
-    print(datuma_paradisana())
+    return f"Sveiks, {vards}!"
